66d1e9d0a53d98f501e37f95373325c2aaa3f1bb
«Vlad» <vladislavgoncear@iesebre.com>
2024-11-26 15:35:40 +0100

    Projecte inicial

--- projecte-landingpage-stockflow/js/main.js
+++ projecte-landingpage-stockflow/js/main.js
@@ -39,3 +39,18 @@ $(document).ready(function () {
       }, ]
   });
 });
+
+
+// Carregar header
+fetch('../components/navbar.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('navbar-container').innerHTML = data;
+});
+
+// Carregar footer
+fetch('../components/footer.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('footer-container').innerHTML = data;
+});
