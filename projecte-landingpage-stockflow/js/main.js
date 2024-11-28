cd746d65dc3a36f6be72b37f9f9f9975e32693d4
«roberto» <rpohrib@iesebre.com>
2024-11-28 19:06:44 +0100

    Preus actualitzats

--- projecte-landingpage-stockflow/js/main.js
+++ projecte-landingpage-stockflow/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  // toggle mobile menu
+  // toggle mobile menu aaaaa
   $('[data-toggle="toggle-nav"]').on('click', function () {
       $(this).closest('nav').find($(this).attr('data-target')).toggleClass('hidden');
       return false;
